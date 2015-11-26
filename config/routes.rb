0c73e7095e356bebf16e3a306761b8b3fdd05c4d
Aron Strandberg <strandberg.aron@gmail.com>
2015-11-26 04:12:06 +0100

    change edit route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'drinklists#index'
 
-  get 'drinklists/edit_list', to: 'drinklists#edit_list', as: 'edit_list'
-  post 'drinklist/edit_list', to: 'drinklists#save_list', as: 'save_list'
+  get 'edit', to: 'drinklists#edit_list', as: 'edit_list'
+  post 'edit', to: 'drinklists#save_list', as: 'save_list'
   resources :drinklists
 end
