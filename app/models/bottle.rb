ef31c7244f49e789ca0b0fda2ce482f2c71b6129
Aron Strandberg <strandberg.aron@gmail.com>
2016-03-02 20:18:39 +0100

    fix sale price formula

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -2,6 +2,6 @@ class Bottle < ActiveRecord::Base
   before_save :fix_price
 
   def fix_price
-    self.price = ((purchase_price * 1.25) / 5).floor * 5
+    self.price = ((purchase_price * 1.25 + 3) / 5).ceil * 5
   end
 end
